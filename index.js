25bb5df8643a4419fc3f9c77b3bb2c27a1311249
kishanghosh090 <kishanghosh090@gamil.com>
2024-05-21 10:18:31 +0530

    my-portfolio

---
+++ index.js
@@ -0,0 +1,6 @@
+let menuicon = document.querySelector("#menu-icon")
+let resmenu = document.querySelector(".res-menu")
+
+menuicon.addEventListener("click", () => {
+    resmenu.classList.toggle("res-menu")
+})
