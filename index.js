86389618951f05310768943d2292bb9b2342587c
kishanghosh090 <kishanghosh090@gamil.com>
2024-05-24 14:12:56 +0530

    bug fix

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 let menuicon = document.querySelector("#menu-icon")
 let resmenu = document.querySelector(".res-menu")
+let opacity = document.querySelector(".opacity2")
 
 menuicon.addEventListener("click", () => {
     resmenu.classList.toggle("res-menu")
+    opacity.classList.toggle("opacity2")
 })
